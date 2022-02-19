766502a238fafb93410b56baf2cb7929e6cc3f41
ACb0y <511108862@qq.com>
2022-02-19 12:03:26 +0800

    代码归档

--- main.cpp
+++ main.cpp
@@ -7,15 +7,17 @@
 using namespace std;
 
 void routine1(void * arg) {
-  cout << "routine1 run" << endl;
+  cout << "routine1 run, value = " << *(int *)(arg) << endl;
 }
 
 void routine2(void * arg) {
-  cout << "routine2 run" << endl;
+  cout << "routine2 run, value = " << *(int *)(arg) << endl;
 }
 
 int main() {
   MyCoroutine::Schedule schedule;
+  int a = 10;
+  int b = 100;
   MyCoroutine::CoroutineCreate(schedule, routine1, &a);
   MyCoroutine::CoroutineCreate(schedule, routine2, &b);
   return 0;
