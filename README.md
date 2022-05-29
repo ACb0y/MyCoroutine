b2425ecf780388a53c3d26c188214223a132994b
ACb0y <511108862@qq.com>
2022-05-29 17:29:41 +0800

    完成单元测试代码的编写

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
-# 简介
-> 使用getcontext，makecontext，setcontext，swapcontext构建一个简单的协程库
-# 待办
-- [x] 实现带优先级的协程调度 
-- [ ] 添加单元测试用例
+# 1.简介
+> 使用getcontext，makecontext，swapcontext构建一个简单的带优先级的协程库
+
+# 2.使用
+- 直接在项目中引用coroutine.h和coroutine.cpp；
+- test目录为单元测试代码，执行`make && ./coroutine_test`，用于执行单元测试代码。
--- test/coroutine_test.cpp
+++ test/coroutine_test.cpp
@@ -12,7 +12,9 @@ using namespace std;
 TEST_CASE(scheduleInit) {
   MyCoroutine::Schedule schedule;
   ASSERT_EQ(MyCoroutine::ScheduleInit(schedule, 1), 0);
+  MyCoroutine::ScheduleClean(schedule);
   ASSERT_EQ(MyCoroutine::ScheduleInit(schedule, 1024), 0);
+  MyCoroutine::ScheduleClean(schedule);
 }
 
 TEST_CASE(ScheduleClean) {
@@ -23,7 +25,9 @@ TEST_CASE(ScheduleClean) {
 
 void fun1(void *arg) {
   MyCoroutine::Schedule *schedule = (MyCoroutine::Schedule *)arg;
+  cout << "fun1 begin" << endl;
   MyCoroutine::CoroutineYield(*schedule);
+  cout << "fun1 end" << endl;
 }
 
 TEST_CASE(ScheduleRunning) {
@@ -35,18 +39,65 @@ TEST_CASE(ScheduleRunning) {
   ASSERT_TRUE(MyCoroutine::ScheduleRunning(schedule));
   ASSERT_EQ(MyCoroutine::CoroutineResumeById(schedule, id), MyCoroutine::Success);
   ASSERT_FALSE(MyCoroutine::ScheduleRunning(schedule));
+  MyCoroutine::ScheduleClean(schedule);
 }
-/*
- * / 创建协程并运行，只能在主协程中调用
-int CoroutineCreate(Schedule& schedule, Entry entry, void* arg, uint32_t priority = 0);
-// 让出执行权，只能在从协程中调用
-void CoroutineYield(Schedule& schedule);
-// 恢复从协程的调用，只能在主协程中调用
-int CoroutineResume(Schedule& schedule);
-// 恢复指定从协程的调用，只能在主协程中调用
-int CoroutineResumeById(Schedule& schedule, int id);
 
-// 判断是否还有协程在运行
-bool ScheduleRunning(Schedule& schedule);
- */
+TEST_CASE(CoroutineResume_Success) {
+  MyCoroutine::Schedule schedule;
+  ASSERT_EQ(MyCoroutine::ScheduleInit(schedule, 100), 0);
+  ASSERT_FALSE(MyCoroutine::ScheduleRunning(schedule));
+  int id = MyCoroutine::CoroutineCreate(schedule, fun1, (void *)&schedule);
+  ASSERT_EQ(id, 0);
+  ASSERT_TRUE(MyCoroutine::ScheduleRunning(schedule));
+  ASSERT_EQ(MyCoroutine::CoroutineResume(schedule), MyCoroutine::Success);
+  ASSERT_FALSE(MyCoroutine::ScheduleRunning(schedule));
+  MyCoroutine::ScheduleClean(schedule);
+}
+
+TEST_CASE(CoroutineResume_InvalidId) {
+  MyCoroutine::Schedule schedule;
+  ASSERT_EQ(MyCoroutine::ScheduleInit(schedule, 100), 0);
+  ASSERT_FALSE(MyCoroutine::ScheduleRunning(schedule));
+  ASSERT_EQ(MyCoroutine::CoroutineResume(schedule), MyCoroutine::InvalidId);
+  MyCoroutine::ScheduleClean(schedule);
+}
+
+TEST_CASE(CoroutineResumeById_SuccessAndNotSuspend) {
+  MyCoroutine::Schedule schedule;
+  ASSERT_EQ(MyCoroutine::ScheduleInit(schedule, 100), 0);
+  ASSERT_FALSE(MyCoroutine::ScheduleRunning(schedule));
+  int id = MyCoroutine::CoroutineCreate(schedule, fun1, (void *)&schedule);
+  ASSERT_EQ(id, 0);
+  ASSERT_TRUE(MyCoroutine::ScheduleRunning(schedule));
+  ASSERT_EQ(MyCoroutine::CoroutineResumeById(schedule, 0), MyCoroutine::Success);
+  ASSERT_FALSE(MyCoroutine::ScheduleRunning(schedule));
+  ASSERT_EQ(MyCoroutine::CoroutineResumeById(schedule, 0), MyCoroutine::NotSuspend);
+  MyCoroutine::ScheduleClean(schedule);
+}
+
+void fun2(void *arg) {
+  MyCoroutine::Schedule *schedule = (MyCoroutine::Schedule *)arg;
+  cout << "fun2 begin" << endl;
+  for (int i = 0; i < 5; i++) {
+    MyCoroutine::CoroutineYield(*schedule);
+    cout << "fun2 yield step " << i + 1 << endl;
+  }
+  cout << "fun2 end" << endl;
+}
+
+TEST_CASE(CoroutineYield) {
+  MyCoroutine::Schedule schedule;
+  ASSERT_EQ(MyCoroutine::ScheduleInit(schedule, 100), 0);
+  ASSERT_FALSE(MyCoroutine::ScheduleRunning(schedule));
+  int id = MyCoroutine::CoroutineCreate(schedule, fun2, (void *)&schedule);
+  ASSERT_EQ(id, 0);
+
+  while (MyCoroutine::ScheduleRunning(schedule)) {
+    ASSERT_EQ(MyCoroutine::CoroutineResumeById(schedule, 0), MyCoroutine::Success);
+    cout << "in master coroutine" << endl;
+  }
+
+  MyCoroutine::ScheduleClean(schedule);
+}
+
 RUN_ALL_TESTS();
